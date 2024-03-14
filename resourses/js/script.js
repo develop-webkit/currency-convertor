e20c7721ddcb4cc85faaeb6540b9d65f88faaec3
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-03-15 03:19:39 +0500

    1.5

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -2,8 +2,6 @@
 
 const currenciesURL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 const countryURL = "../resourses/js/country.json";
-const curURL = "../resourses/js/cur.js";
-
 
 const fromCurDiv = document.querySelector("#fromCurDiv");
 const toCurDiv = document.querySelector("#toCurDiv");
@@ -12,18 +10,22 @@ const toCurList = document.querySelector("#toCurList");
 const fromCurList = document.querySelector("#fromCurList");
 
 const exchangeIcon = document.querySelector("#exchange-icon");
+const submitBtn = document.querySelector("#submit-btn");
 
+const fromCurrentName = document.querySelector("#fromCurrentName");
+const singleExchangeRate = document.querySelector("#singleExchangeRate");
+const toCurrentName = document.querySelector("#toCurrentName");
+const toCurrentRateFull = document.querySelector("#toCurrentRateFull");
+const toCurrentNameFull = document.querySelector("#toCurrentNameFull");
+const inputNumber = document.querySelector("#input-number");
 
-let fromCurListItems = "gg";
-let toCurListItems = "gg";
+let fromCurListItems = "";
+let toCurListItems = "";
 
 const fromCur = "US";
 const toCur = "PK";
 
-(async () => {
-
-    const responsesCur = await fetch(`${currenciesURL}.json`);
-    const currencies = await responsesCur.json();
+(() => {
     let listItems = "";
     let fromCurDivInner = ""
     let toCurDivInner = ""
@@ -33,11 +35,11 @@ const toCur = "PK";
         listItems += `<li data-cur="${countryList[key]}"> <span class="span-cur">${key}</span> <img class="flag-img" src="https://flagsapi.com/${countryList[key]}/flat/64.png" alt="flag of "> </li>`;
 
         if(countryList[key] === fromCur){
-            fromCurDivInner = ` <span class="span-cur" data-cur="${countryList[key]}">${key}</span> <img class="flag-img" src="https://flagsapi.com/${countryList[key]}/flat/64.png" alt="flag of "> <img class="down-arrow-icon" src="resourses/img/down.png" alt="down arrow icon">`;
+            fromCurDivInner = ` <span class="span-cur" data-cur="${key}" data-code="${countryList[key]}">${key}</span> <img class="flag-img" src="https://flagsapi.com/${countryList[key]}/flat/64.png" alt="flag of "> <img class="down-arrow-icon" src="resourses/img/down.png" alt="down arrow icon">`;
         }
 
         if(countryList[key] === toCur){
-            toCurDivInner = ` <span class="span-cur" data-cur="${countryList[key]}">${key}</span> <img class="flag-img" src="https://flagsapi.com/${countryList[key]}/flat/64.png" alt="flag of "> <img class="down-arrow-icon" src="resourses/img/down.png" alt="down arrow icon">`;
+            toCurDivInner = ` <span class="span-cur" data-cur="${key}" data-code="${countryList[key]}">${key}</span> <img class="flag-img" src="https://flagsapi.com/${countryList[key]}/flat/64.png" alt="flag of "> <img class="down-arrow-icon" src="resourses/img/down.png" alt="down arrow icon">`;
         }
     }
 
@@ -49,6 +51,7 @@ const toCur = "PK";
 
     fromCurListItems = Array.from(document.querySelectorAll("#fromCurList li"));
     toCurListItems = Array.from(document.querySelectorAll("#toCurList li"));
+
 })();
 
 
@@ -57,7 +60,7 @@ setInterval( () =>{
         fromCurListItem.addEventListener('click',(e)=>{
                 const keyValue = e.currentTarget.dataset.cur;
                 const  key = Object.keys(countryList).find(k=>countryList[k]===keyValue);
-                fromCurDiv.innerHTML = ` <span class="span-cur" data-cur="${key}">${key}</span> <img class="flag-img" src="https://flagsapi.com/${keyValue}/flat/64.png" alt="flag of "> <img class="down-arrow-icon" src="resourses/img/down.png" alt="down arrow icon">`;
+                fromCurDiv.innerHTML = ` <span class="span-cur" data-cur="${key}" data-code="${keyValue}">${key}</span> <img class="flag-img" src="https://flagsapi.com/${keyValue}/flat/64.png" alt="flag of "> <img class="down-arrow-icon" src="resourses/img/down.png" alt="down arrow icon">`;
         });
     }
 
@@ -65,7 +68,7 @@ setInterval( () =>{
         toCurListItem.addEventListener('click',(e)=>{
                 const keyValue = e.currentTarget.dataset.cur;
                 const  key = Object.keys(countryList).find(k=>countryList[k]===keyValue);
-                toCurDiv.innerHTML = ` <span class="span-cur" data-cur="${key}">${key}</span> <img class="flag-img" src="https://flagsapi.com/${keyValue}/flat/64.png" alt="flag of "> <img class="down-arrow-icon" src="resourses/img/down.png" alt="down arrow icon">`;
+                toCurDiv.innerHTML = ` <span class="span-cur" data-cur="${key}" data-code="${keyValue}">${key}</span> <img class="flag-img" src="https://flagsapi.com/${keyValue}/flat/64.png" alt="flag of "> <img class="down-arrow-icon" src="resourses/img/down.png" alt="down arrow icon">`;
         });
     }
 }, 500);
@@ -99,5 +102,24 @@ exchangeIcon.addEventListener('click', () =>{
     toCurDiv.innerHTML = repRaplce;
 });
 
+async function updateCurrency(){
+    const responsesCurFrom = await fetch(`${currenciesURL}/${(fromCurDiv.firstElementChild.dataset.cur).toLowerCase()}.json`);
+    const resCurList = await responsesCurFrom.json();
+
+    const resContryURl = await fetch(countryURL);
+    const countryCodes = await resContryURl.json();
+
+    const fromCurrency = fromCurDiv.firstElementChild.dataset.cur.toLowerCase();
+    const toCurrency = toCurDiv.firstElementChild.dataset.cur.toLowerCase();
+
+    fromCurrentName.textContent = fromCurDiv.firstElementChild.dataset.cur;
+    singleExchangeRate.textContent = (resCurList[fromCurrency][toCurrency]).toFixed(2);
+    toCurrentName.textContent = toCurDiv.firstElementChild.dataset.cur;
+    toCurrentRateFull.textContent = (resCurList[fromCurrency][toCurrency] * inputNumber.value).toLocaleString('en-US', {maximumFractionDigits:2});
+    toCurrentNameFull.textContent = (countryCodes[toCurDiv.firstElementChild.dataset.code.toLowerCase()]["currency_name"]).toUpperCase();
+}
 
 
+submitBtn.addEventListener('click', updateCurrency);
+
+updateCurrency();
